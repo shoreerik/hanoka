c76aa8a7682f748f1ebaf857056728714c1fdb12
ghm1979 <galit.menahem79@gmail.com>
2023-12-13 03:02:33 +0200

    JSON 3

---
+++ data/classes.js
@@ -0,0 +1,9 @@
+export default class User {
+    constructor(id,fname, lname, email, password) {
+        this.id=id;
+        this.fname = fname;
+        this.lname = lname;               
+        this.email = email;
+        this.password = password;
+    }
+}
